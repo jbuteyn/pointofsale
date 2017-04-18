fbaeeb4c45da32b9c98417f20fc8053ef288b511
Josh Buteyn <jbuteyn5@gmail.com>
2017-04-18 11:12:06 -0400

    added code to MakeSelections, added receipt class which still needs a little work

---
+++ pointofsale/receipt.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pointofsale
+{
+    class receipt
+    {
+        List<string> userReceipt = new List<string>();
+        double totalPrice = 0;
+        
+        public void addToReceipt(string addItem)
+        {
+            userReceipt.Add(addItem);
+
+        } 
+        
+        public void printReceipt()
+        {
+            for (int i = 0; i< userReceipt.Count; i++)
+			{
+                Console.WriteLine(userReceipt[i]);
+			}
+        
+        
+        }
+        public void addPrice(double price)
+        {
+
+            totalPrice = totalPrice + price;
+
+        }
+        public double returnTotal()
+        {
+
+
+            return totalPrice;
+
+        }
+    }
+
+}
